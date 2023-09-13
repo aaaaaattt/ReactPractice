ec20b55ec5fa0b00e4b07fd82f623f075a5a0215
ch <chlgur13@kakao.com>
2023-09-13 19:47:18 +0900

    0913Wed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef,useState} from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+
+  const {username, email} = inputs;
+  const onChange = e => {
+  const {name, value} = e.target;
+  //console.log(e.target);
+    setInputs({
+      ...inputs,
+      [name]:value
+    });
+    //console.log(inputs); 
+  };
+
+  const [users, setUsers] = useState([
+    {
+        id : 1,
+        username: 'a',
+        email : 'a@naver.com',
+        active: true
+    },
+    {
+        id : 2,
+        username: 'b',
+        email : 'b@naver.com',
+        active: false
+    },
+    {
+        id : 3,
+        username: 'c',
+        email : 'c@naver.com',
+        active: false
+    }
+  ]);
+  
+  const nextId = useRef(4);
+  
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user));
+
+    setInputs({
+      username:'',
+      email:''
+    });
+    nextId.current += 1;
+  }
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user => 
+        user.id === id ? {...user, active: !user.active} : user)
+    )
+  }
+
+  return (  
+    <>
+    <CreateUser
+      username={username}  
+      email={email}
+      onChange={onChange}
+      onCreate={onCreate}
+    />
+    <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    </>
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello({name, color, isSpecial}){
+
+    return <div style={{color}}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+        </div>
+}
+
+export default Hello;
