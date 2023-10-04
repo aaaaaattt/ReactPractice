81b442caecd4a27076a0d8b7745dbbca61c69f10
ch <chlgur13@kakao.com>
2023-10-04 09:38:34 +0900

    using useEffect for study

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React,{useEffect} from "react";
 
 function User({user, onRemove, onToggle}){
+    useEffect(() => { // deps 안에 특정 값이 있다면 언마운트시에도 호출이되고,
+                        //값이 바뀌기 직전에도 호출
+        console.log(user)
+    })
+
+
     return(
         <div>
             <b
